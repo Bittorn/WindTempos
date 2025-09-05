ada1d67b0c3416aaa27a928f5780a6bbb0dc0628
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-05 14:06:15 +1000

    new GitHub card

--- src/routes/tows/+page.svelte
+++ src/routes/tows/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import towsLogo from '$lib/assets/tows-icon.png';
-	import styles from "./tows.module.scss"
+	import TowsCard from '$lib/components/TOWSCard.svelte';
+	import styles from './tows.module.scss';
 </script>
 
 <h1 class="funnel-display">Tales of Wayward Stars</h1>
+<TowsCard></TowsCard>
 <img src={towsLogo} alt="Tales of Wayward Stars logo" class={styles.center} />
-<h2>Under development</h2>
-<p>You can view current progress on <a href='https://github.com/Bittorn/TOWS-Client'>the GitHub</a></p>
