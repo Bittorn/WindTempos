75e0670f794fdd4b16450ba18bbfe7295f26fb34
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-13 21:46:38 +1000

    Added social icons

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-    import logo from '$lib/assets/logo-horizontal-invert.png';
+	import logo from '$lib/assets/logo-horizontal-invert.png';
+	import discordIcon from '$lib/assets/discord-icon.svg'
+	import githubIcon from '$lib/assets/github-icon.svg'
 	import NavLink from './NavLink.svelte';
-    import styles from './NavBar.module.scss';
+	import styles from './NavBar.module.scss';
+
+	const discordLink = "https://discord.gg/FHbCJDx4";
+	const githubLink = "https://github.com/Bittorn/WindTempos";
 </script>
 
 <nav class="{styles.navbar} funnel-display">
 	<div class={styles.logo}>
-		<a href='/'><img src={logo} alt="Logo" /></a>
+		<a href="/"><img src={logo} alt="Logo" /></a>
 	</div>
 	<ul>
-		<NavLink href='/'>Home</NavLink>
-		<NavLink href='/about'>About</NavLink>
-		<NavLink href='/tows'>TOWS</NavLink>
-		<NavLink href='/projects'>Projects</NavLink>
-		<NavLink href='/contact'>Contact</NavLink>
+		<NavLink href="/">Home</NavLink>
+		<NavLink href="/about">About</NavLink>
+		<NavLink href="/tows">TOWS</NavLink>
+		<NavLink href="/projects">Projects</NavLink>
+		<NavLink href="/contact">Contact</NavLink>
 	</ul>
+	<div class={styles.socials}>
+		<a href={discordLink} target="_blank"><img src={discordIcon} alt="Discord icon" /></a>
+		<a href={githubLink} target="_blank"><img src={githubIcon} alt="GitHub icon" /></a>
+	</div>
 </nav>
